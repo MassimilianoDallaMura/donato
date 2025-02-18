ffdaa147b5baaa9aa024fadb1ee8496d4857a681
Massimiliano Dalla Mura <dalla.mura91@gmail.com>
2025-02-18 11:49:19 +0100

    corsfilter

--- src/main/java/Backend/DonaTo/security/Config.java
+++ src/main/java/Backend/DonaTo/security/Config.java
@@ -48,7 +48,7 @@ public class Config {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "https://app-donato.web.app", "https://distinct-josefa-massimilianodm-55a6219c.koyeb.app"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200", "https://donato-applicazione.web.app", "https://distinct-josefa-massimilianodm-55a6219c.koyeb.app"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true); // Se vuoi permettere l'invio di cookie
